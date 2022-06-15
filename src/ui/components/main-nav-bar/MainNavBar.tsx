0005e2865fc4a0cbb53435094719cb705a821294
Karol Kamiński <>
2022-06-15 10:02:14 +0200

    yarn shakalaka

---
+++ src/ui/components/main-nav-bar/MainNavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import s from "./MainNavBar.module.scss"
+
+export const MainNavBar = () => {
+    return (
+      <div className={s.elo}>
+          NAVBAR
+      </div>
+    )
+}
